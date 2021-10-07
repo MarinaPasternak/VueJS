6684e811509aa4b942e7b55bf291e003e9d49b73
Marina_Pasternak <31963187+MarinaPasternak@users.noreply.github.com>
2021-10-07 17:39:30 +0300

    updated with Vue3

--- GoalList/app.js
+++ GoalList/app.js
@@ -1,13 +1,15 @@
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
+const app = Vue.createApp({
+    data() {
+      return {
+        goals: [],
+        enterdValue: ''
+      };
+    },
 
-function addGoal() {
-  const enteredValue = inputEl.value;
-  const listItemEl = document.createElement('li');
-  listItemEl.textContent = enteredValue;
-  listEl.appendChild(listItemEl);
-  inputEl.value = '';
-}
-
-buttonEl.addEventListener('click', addGoal);
+    methods: {
+      addGoal() {
+        this.goals.push(this.enterdValue);
+        this.enterdValue = '';
+      }
+    }
+  }).mount('#app');
