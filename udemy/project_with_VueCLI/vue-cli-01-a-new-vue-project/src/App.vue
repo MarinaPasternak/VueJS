d7c8e7c4325bde910221273517665a9a4f7a7a31
Marina Pasternak <256pasternakmarina@gmail.com>
2021-11-01 13:34:41 +0200

    added friend contatc component

---
+++ udemy/project_with_VueCLI/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <section>
+        <h2>My Friends</h2>
+        <ul>
+            <friend-contact></friend-contact>
+            <friend-contact></friend-contact>
+        </ul>
+    </section>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                friends: [
+                    {
+                        id: 'manuel',
+                        friendName: 'Jane July',
+                        phone: '121 231 23 23',
+                        email: 'test@gmail.com'
+                    },
+                    {
+                        id: 'manuel',
+                        friendName: 'Lucu July',
+                        phone: '234 555 23',
+                        email: 'test122312312@gmail.com'
+                    },
+                ]
+            };
+        },
+        methods: {},
+        computed: {}
+    };
+</script>
---
+++ udemy/project_with_VueCLI/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,32 @@
+<template>
+   <li>
+       <h2>{{ friend.friendName }}</h2>
+       <button @click="toogleDetails">Show Details</button>
+        <ul v-if="datailsVisibility">
+            <li><strong>Phone:</strong>{{ friend.phone }}</li>
+            <li><strong>Email:</strong>{{ friend.email }}</li>
+        </ul>
+   </li>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                datailsVisibility: false,
+                friend: {
+                    id: 'manuel',
+                    friendName: 'Jane July',
+                    phone: '121 231 23 23',
+                    email: 'test@gmail.com'
+                }
+            };
+        },
+        methods: {
+            toogleDetails() {
+                this.datailsVisibility = !this.datailsVisibility;
+            }
+        },
+        computed: {}
+    };
+</script>
