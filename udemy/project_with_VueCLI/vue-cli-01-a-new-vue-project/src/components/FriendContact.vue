ea6edeca93ca7d1371b59a315b61d59fbd41b6b7
Marina Pasternak <256pasternakmarina@gmail.com>
2021-11-04 15:27:33 +0200

    added validation for friend contact

--- udemy/project_with_VueCLI/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
+++ udemy/project_with_VueCLI/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -1,16 +1,35 @@
 <template>
    <li>
-       <h2>{{ friend.friendName }}</h2>
+       <h2>{{ name }}</h2>
        <button @click="toogleDetails">{{ detailsVisibility ? 'Hide' : 'Show' }} Details</button>
         <ul v-if="detailsVisibility">
-            <li><strong>Phone:</strong>{{ friend.phone }}</li>
-            <li><strong>Email:</strong>{{ friend.email }}</li>
+            <li><strong>Phone:</strong>{{ phoneNumber }}</li>
+            <li><strong>Email:</strong>{{ emailAddress }}</li>
         </ul>
    </li>
 </template>
 
 <script>
     export default {
+        props: {
+            'name': {
+                type: String,
+                required:true
+            },
+            'phoneNumber': {
+                type: String,
+                required:true
+            },
+            'emailAddress': {
+                type: String,
+                required:true
+            },
+            isFavourite: {
+                type: Boolean,
+                required:true,
+                default: false
+            }
+        },
         data() {
             return {
                 detailsVisibility: false,
