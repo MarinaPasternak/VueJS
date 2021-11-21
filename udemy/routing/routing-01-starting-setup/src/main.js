404c34810bfb39bc1559029b09e12f8135a73089
Marina Pasternak <256pasternakmarina@gmail.com>
2021-11-21 22:26:26 +0200

    added routing projects

---
+++ udemy/routing/routing-01-starting-setup/src/main.js
@@ -0,0 +1,51 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            redirect: '/teams'
+        },
+        {
+            path: '/teams',
+            component: TeamsList,
+            children: [
+                {
+                    name: 'team-members',
+                    path: ':teamId',
+                    component: TeamMembers,
+                    props:true
+                },
+            ]
+        },
+        {
+            path: '/users',
+            component: UsersList
+        },
+        {
+            path: '/:catchAll(.*)',
+            redirect: '/teams'
+        },
+    ],
+    scrollBehavior(to, from, saveposition) {
+        console.log(to, from, saveposition);
+        if(saveposition) {
+            return saveposition
+        } else {
+            return {left: 0, top:0};
+        }
+    }
+});
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
