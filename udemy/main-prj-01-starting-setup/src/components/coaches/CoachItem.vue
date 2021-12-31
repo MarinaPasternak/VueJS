6f4444d9e2cf9d9e99dcee6414b6f146e3cd135e
Marina Pasternak <256pasternakmarina@gmail.com>
2021-12-31 16:29:29 +0200

    added base components

--- udemy/main-prj-01-starting-setup/src/components/coaches/CoachItem.vue
+++ udemy/main-prj-01-starting-setup/src/components/coaches/CoachItem.vue
@@ -3,20 +3,25 @@
         <h3>{{ fullName }}</h3>
         <h4> ${{ rate }}</h4>
         <div>
-            <span v-for="area in areas" :key="area">
-                {{ area }}
-            </span>
+            <base-badge v-for="area in areas" 
+              :key="area" 
+              :type=area 
+              :title="area"
+            >{{ area }}
+            </base-badge>
         </div>
         <div class="actions">
-            <router-link :to="coachContactLink">Contact</router-link>
-            <router-link :to="coachDetailLink">View Detail</router-link>
+            <base-button mode="outline" link :to="coachContactLink">Contact</base-button>
+            <base-button link :to="coachDetailLink">View Detail</base-button >
         </div>
     </li>
 </template>
 
 <script>
+import BaseButton from '../ui/BaseButton.vue';
 
 export default {
+  components: { BaseButton },
     props: ['id', 'firstName','lastName', 'rate', 'areas'],
     computed: {
         fullName() {
