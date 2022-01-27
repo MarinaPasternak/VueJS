847d1dce5c1edcc5754c3f86c07c69d9a0a9270f
Marina Pasternak <256pasternakmarina@gmail.com>
2022-01-27 23:37:39 +0200

    added coach register form

--- udemy/main-prj-01-starting-setup/src/store/index.js
+++ udemy/main-prj-01-starting-setup/src/store/index.js
@@ -5,6 +5,16 @@ import coachesModule from './modules/coaches/index.js';
 const store = createStore({
     modules: {
         coaches: coachesModule
+    },
+    state() {
+        return {
+            userID: 'c3'
+        };
+    },
+    getters: {
+        userID(state) {
+            return state.userID;
+        }
     }
 });
 
